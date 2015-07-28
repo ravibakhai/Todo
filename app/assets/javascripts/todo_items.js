7dcdca3cc120d0e507cf67d25f7bde351c49d7e0
Ravi Bakhai <ravi.bakhai@gmail.com>
2015-07-28 15:19:12 -0400

    Edited Todo List

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -1,7 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-// var main = function() {
-//   $('.main').click(function() {
-//   $(this).next().toggle();
-// });
+// var main = function () {
+//     $('.main').onClick(function () {
+//         $(this).toggle();
+//         console.log("hello");
+//     });
+// };
+var main = function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+};
+$(document).ready(main);
